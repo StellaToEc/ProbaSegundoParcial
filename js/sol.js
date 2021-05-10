8b262ebf44dc2b0816531ef35a1d55f29b8fac5b
BrandMV <brandonmv2001@gmail.com>
2021-05-10 18:01:33 -0500

    Add probability

--- js/sol.js
+++ js/sol.js
@@ -1,26 +1,39 @@
-let bandera=0;
-let utilidad;
-const solucionar = () =>{
-    x = document.getElementById("x").value
-    y = document.getElementById("y").value
-    z = document.getElementById("z").value
-    console.log(x);
-    utilidad = (0.22)*x + (0.36)*y + (0.14)*z
-}
+let bandera = 0;
+let utilidad = 0;
+let probabilidad = 0;
+const solucionar = () => {
+  x = document.getElementById("x").value;
+  y = document.getElementById("y").value;
+  z = document.getElementById("z").value * -1;
+  let a = document.getElementById("a").value;
+  let b = document.getElementById("b").value;
+  let c = document.getElementById("c").value;
+  let d = document.getElementById("d").value;
 
+  probabilidad = parseFloat(a) + parseFloat(b) + parseFloat(c) + parseFloat(d);
+//   console.log(probabilidad);
+  if (probabilidad !== 1) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      html: `La suma de las probabilidades debe ser 1</p>`,
+    });
+    bandera = 1;
+  }
+  utilidad = a * x + b * y + c * 0 + d * z;
+//   console.log(utilidad);
+};
 
 const formulario = document.getElementById("sol");
-formulario.addEventListener('submit', (event) => {
-    bandera=0;
-    event.preventDefault();
-    solucionar()
-    if(bandera===0){
-        Swal.fire({
-            icon: 'success',
-            title: 'Resultados',
-            html:  `<p>La utilidad esperada con los datos ingresados es de: ${utilidad}$</p>`
-            
-        })
-    }
-    
+formulario.addEventListener("submit", (event) => {
+  bandera = 0;
+  event.preventDefault();
+  solucionar();
+  if (bandera === 0) {
+    Swal.fire({
+      icon: "success",
+      title: "Resultados",
+      html: `<p>La utilidad esperada con los datos ingresados es de: ${utilidad}$</p>`,
+    });
+  }
 });
