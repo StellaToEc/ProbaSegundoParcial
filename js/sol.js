0d3a11d9b50aed88b74ed7a5243e7ca45a0f78c0
AnFo <angelgabriel1020@gmail.com>
2021-05-12 16:33:29 -0500

    graph fea anadida

--- js/sol.js
+++ js/sol.js
@@ -1,18 +1,26 @@
 let bandera = 0;
 let utilidad = 0;
 let probabilidad = 0;
+
+var ag,bg,cg,dg;
+
+google.charts.load('current', {'packages':['bar']});
+google.charts.setOnLoadCallback(drawChart);
+
 const solucionar = () => {
   x = document.getElementById("x").value;
   y = document.getElementById("y").value;
   z = document.getElementById("z").value * -1;
+
   let a = document.getElementById("a").value;
   let b = document.getElementById("b").value;
   let c = document.getElementById("c").value;
   let d = document.getElementById("d").value;
-
+  ag=a; bg=b;cg=c;dg=d;
+  
   probabilidad = parseFloat(a) + parseFloat(b) + parseFloat(c) + parseFloat(d);
-//   console.log(probabilidad);
-  if (probabilidad !== 1) {
+   console.log(probabilidad);
+  if (probabilidad == 1) {
     Swal.fire({
       icon: "error",
       title: "Error",
@@ -25,15 +33,69 @@ const solucionar = () => {
 };
 
 const formulario = document.getElementById("sol");
+
 formulario.addEventListener("submit", (event) => {
   bandera = 0;
   event.preventDefault();
   solucionar();
   if (bandera === 0) {
+    
+    //const graph = document.querySelector(".grapScript");
     Swal.fire({
       icon: "success",
       title: "Resultados",
-      html: `<p>La utilidad esperada con los datos ingresados es de: ${utilidad}$</p>`,
+      html: `<p>La utilidad esperada con los datos ingresados es de: ${utilidad}$</p>
+      <div id="columnchart_material" style="width: 800px; height: 500px; margin:50px;"></div>`,
     });
+    //graph.innerHTML = '';
+    drawChart();
+
   }
 });
+
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Utilidad', 'Probabilidad'],
+  ['d', dg*100],
+  ['c', cg*100],
+  ['b', bg*100],
+  ['a', ag*100],
+  ]);
+
+  var options = {
+  chart: {
+      title: 'Gráfica de las probabilidades',
+  },
+  vAxis: {
+      title: 'Probabilidad en %'
+    }
+  };
+
+  var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+}
+
+/*function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Utilidad', 'Probabilidad', 'Prob. frecuentista (sim) __'],
+  ['d', dg*100, 15],
+  ['c', cg*100, 25],
+  ['b', bg*100, 35],
+  ['a', ag*100, 35],
+  ]);
+
+  var options = {
+  chart: {
+      title: 'Gráfica de las probabilidades',
+  },
+  vAxis: {
+      title: 'Probabilidad en %'
+    }
+  };
+
+  var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+}*/
